750124ff150696c8c724b6c1a93d6feb45de0125
Sean Murphy <filmmakersean@fastmail.net>
2019-04-05 06:02:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,12 @@
-count = 0
 def turn_count(board)
+  count = 0
   board.each do |move|
     if move == "X" || "O"
       count += 1
     end
   end
+  return count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
